9ee67d56fcdc32ef4409ba3a5fa77eccf24ae428
EricJGardner <74524075+EricJGardner@users.noreply.github.com>
2021-02-10 08:37:39 -0600

    initializing

---
+++ src/test/java/tts/com/techtalentblog/TechtalentblogApplicationTests.java
@@ -0,0 +1,13 @@
+package tts.com.techtalentblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechtalentblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
